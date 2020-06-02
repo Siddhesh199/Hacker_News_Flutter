c53d99b6fafa5f74fc9f9019c3d7c7cf990d8b79
Siddhesh199 <siddheshsalaskar188@gmail.com>
2020-06-02 13:57:18 +0530

    Created Repository for Providers

--- lib/src/resources/news_api_provider.dart
+++ lib/src/resources/news_api_provider.dart
@@ -6,16 +6,16 @@ import 'package:http/http.dart' show Client;
 final _root = 'https://hacker-news.firebaseio.com/v0';
 
 class NewsApiProvider {
-  Client client = Client(); 
+  Client client = Client();
 
-  fetchTopIds() async {
+  Future<List<int>> fetchTopIds() async {
     final response = await client.get('$_root/topstories.json');
     final ids = json.decode(response.body);
 
-    return ids;
+    return ids.cast<int>();
   }
 
-  fetchItem(int id) async {
+  Future<ItemModel> fetchItem(int id) async {
     final response = await client.get('$_root/item/$id.json');
     final parsedJson = json.decode(response.body);
 
