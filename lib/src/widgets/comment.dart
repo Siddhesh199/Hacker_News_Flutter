d13afd9ae782cdfa9d7c3d3d5057c71a2eb0b062
Siddhesh199 <siddheshsalaskar188@gmail.com>
2020-06-05 22:33:16 +0530

    Handled deleted comments

--- lib/src/widgets/comment.dart
+++ lib/src/widgets/comment.dart
@@ -16,7 +16,26 @@ class Comment extends StatelessWidget {
           return Text('Still loading comment');
         }
 
-        return Text(snapshot.data.text);
+        final item = snapshot.data;
+
+        final children = <Widget>[
+          ListTile(
+            title: Text(item.text),
+            subtitle: item.by == '' ? Text('Deleted') : Text(item.by),
+          ),
+          Divider(),
+        ];
+
+        item.kids.forEach((kidId) {
+          children.add(
+            Comment(
+              itemId: kidId,
+              itemMap: itemMap,
+            ),
+          );
+        });
+
+        return Column(children: children);
       },
     );
   }
