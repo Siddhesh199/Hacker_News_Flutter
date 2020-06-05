a411aac1afb7a13322dceca1d92e7b2be4b25658
Siddhesh199 <siddheshsalaskar188@gmail.com>
2020-06-05 23:19:42 +0530

    Replaced HTML characters in comments and refactored

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -21,6 +21,10 @@ class App extends StatelessWidget {
     if (settings.name == '/') {
       return MaterialPageRoute(
         builder: (context) {
+          final storiesBloc = StoriesProvider.of(context);
+
+          storiesBloc.fetchTopIds();
+
           return NewsList();
         },
       );
--- lib/src/widgets/comment.dart
+++ lib/src/widgets/comment.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:hackernewsflutter/src/models/item_model.dart';
+import 'package:hackernewsflutter/src/widgets/loading_container.dart';
 
 class Comment extends StatelessWidget {
   final int itemId;
@@ -14,14 +15,14 @@ class Comment extends StatelessWidget {
       future: itemMap[itemId],
       builder: (context, AsyncSnapshot<ItemModel> snapshot) {
         if (!snapshot.hasData) {
-          return Text('Still loading comment');
+          return LoadingContainer();
         }
 
         final item = snapshot.data;
 
         final children = <Widget>[
           ListTile(
-            title: Text(item.text),
+            title: buildText(item),
             subtitle: item.by == '' ? Text('Deleted') : Text(item.by),
             contentPadding: EdgeInsets.only(
               right: 16.0,
@@ -44,5 +45,14 @@ class Comment extends StatelessWidget {
         return Column(children: children);
       },
     );
+  }
+
+  Widget buildText(ItemModel item) {
+    final text = item.text
+        .replaceAll('&#x27;', "'")
+        .replaceAll('<p>', '\n\n')
+        .replaceAll('</p>', '');
+
+    return Text(text);
   }
 }
